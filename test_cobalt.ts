a82e2d4886a5a150a470aabbac52468b17cc4ab9
Animesh <69345507+n-ce@users.noreply.github.com>
2025-09-12 09:09:42 +0530

    Create test_cobalt.ts

---
+++ test_cobalt.ts
@@ -0,0 +1,39 @@
+const raw = 'https://codeberg.org/kwiat/instances/raw/branch/main/src/lib/input/instances.json';
+
+async function getLink(instance: string): Promise<string | undefined> {
+
+  const id = 'https://youtu.be/tIJVUXICI1c';
+  let dl = '';
+  await fetch(instance, {
+    method: 'POST',
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      url: id,
+      downloadMode: 'audio',
+      audioFormat: 'opus',
+      filenameStyle: 'basic'
+    })
+  })
+    .then(_ => _.json())
+    .then(_ => {
+      if ('url' in _)
+        dl = _.url;
+      else throw new Error(_.error.code);
+    })
+    .catch(console.error);
+
+  return dl;
+
+}
+
+
+export default () =>
+  fetch(raw)
+    .then(res => res.json())
+    .then(async data => {
+      console.log('Start Cobalt Testing...')
+      const instances = data.slice(1).map(_ => 'https://' + _[1]);
+      const promises = Promise.all(instances.map(getLink));
+      return (await promises).filter(Boolean);
+    });
+
